797a9a612cc113f3da5e003f86d44701decb76d6
Giridhar Sakineni <giridhar.sakineni@humanaedge.com>
2020-04-30 15:13:03 -0400

    module2 bug fix and commit

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -28,6 +28,8 @@ namespace WishList
             else
                 app.UseExceptionHandler("/Home/Error");
             app.UseRouting();
+
+            app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
         }
     }
 }
